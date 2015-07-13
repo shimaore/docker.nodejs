cbff896540eeb5bdfd4b9f21448e18c4f397a66d
Stephane Alnet <stephane@shimaore.net>
2015-07-13 14:05:44 +0200

    Tests

--- test/0001.sh
+++ test/0001.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-docker run --rm --name nodejs-test-0001 shimaore/nodejs node -v
-docker run --rm --name nodejs-test-0001 shimaore/nodejs node -e 'var fs = require("fs"); var motd = fs.readFileSync("/etc/motd"); var assert = require("assert"); assert.equal(typeof motd, "object"); assert.ok(motd instanceof Buffer);'
-docker run --rm --name nodejs-test-0001 shimaore/nodejs npm install bluebird
-docker run --rm --name nodejs-test-0001 shimaore/nodejs n latest
-docker run --rm --name nodejs-test-0001 shimaore/nodejs curl -s https://google.com/
-docker run --rm --name nodejs-test-0001 shimaore/nodejs npm install git+https://github.com/quirkey/sammy.git#245dd7f8869b708b31eed552512e9cdf0f0df501
+TAG=`jq -r .version ../package.json`
+docker run --rm --name nodejs-test-0001 shimaore/nodejs:${TAG} node -v
+docker run --rm --name nodejs-test-0001 shimaore/nodejs:${TAG} node -e 'var fs = require("fs"); var motd = fs.readFileSync("/etc/motd"); var assert = require("assert"); assert.equal(typeof motd, "object"); assert.ok(motd instanceof Buffer);'
+docker run --rm --name nodejs-test-0001 shimaore/nodejs:${TAG} npm install bluebird
+docker run --rm --name nodejs-test-0001 shimaore/nodejs:${TAG} n latest
+docker run --rm --name nodejs-test-0001 shimaore/nodejs:${TAG} curl -s https://google.com/
+docker run --rm --name nodejs-test-0001 shimaore/nodejs:${TAG} npm install git+https://github.com/quirkey/sammy.git#245dd7f8869b708b31eed552512e9cdf0f0df501
